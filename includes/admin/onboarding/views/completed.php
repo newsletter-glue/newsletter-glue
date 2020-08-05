b8de42a8135609cb5cc66cf00bdb8147bdb1f9d1
newsletter-glue <68359897+newsletter-glue@users.noreply.github.com>
2020-08-05 18:10:43 +0100

    onboarding ui

--- includes/admin/onboarding/views/completed.php
+++ includes/admin/onboarding/views/completed.php
@@ -16,15 +16,21 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 	<p style="font-size: 36px;">🎉</p>
 
-	<h3 style="padding-bottom: 5px;"><?php _e( 'Set up complete!', 'newsletter-glue' ); ?></h3>
+	<h3><?php _e( 'Set up complete!', 'newsletter-glue' ); ?>
+		<span><?php _e( 'Here&rsquo;s how you can quickly send a post to your subscribers.', 'newsletter-glue' ); ?></span>
+	</h3>
 
-	<h3><?php _e( 'You&rsquo;re ready to send a post to your subscribers.', 'newsletter-glue' ); ?>
-		<span style="margin-top: 24px;"><?php _e( 'We&rsquo;ve created a demo post for you.', 'newsletter-glue' ); ?></span>
-		<span><?php _e( 'Let&rsquo;s head over there now.', 'newsletter-glue' ); ?></span>
+	<div class="ngl-video">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/0LiLb3KKarE?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+
+	<h3>
+		<span style="margin-top: 24px;"><?php _e( 'Now it&rsquo;s your turn.', 'newsletter-glue' ); ?></span>
+		<span><?php _e( 'Try it yourself in this demo post we&rsquo;ve created.', 'newsletter-glue' ); ?></span>
 	</h3>
 
 	<div class="ngl-boarding-btn ngl-btn">
-		<a href="<?php echo add_query_arg( 'ngl_onboarding', 'complete' ); ?>" class="ui primary button ngl-boarding-complete"><?php _e( 'Try demo', 'newsletter-glue' ); ?></a>
+		<a href="<?php echo add_query_arg( 'ngl_onboarding', 'complete' ); ?>" class="ui primary button ngl-boarding-complete"><?php _e( 'Try demo post', 'newsletter-glue' ); ?></a>
 	</div>
 
 </div>
