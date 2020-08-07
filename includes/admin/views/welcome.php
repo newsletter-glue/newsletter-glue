b0a311b92fde23d2b6d29c1b6b4a269715c067b7
newsletter-glue <68359897+newsletter-glue@users.noreply.github.com>
2020-08-07 16:14:26 +0100

    Change onboarding post

--- includes/admin/views/welcome.php
+++ includes/admin/views/welcome.php
@@ -13,247 +13,169 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>Thanks for using Newsletter Glue. We're glad you're here.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:heading -->
-<h2>This post is a hands-on guide to publishing newsletters with WordPress</h2>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph {"customBackgroundColor":"#f1ecff","fontSize":"small","className":"NGpurplebox"} -->
-<p style="background-color:#f1ecff" class="has-background has-small-font-size NGpurplebox">💡 <strong>Since this is hands-on, don't be afraid to play around.</strong><br>Try stuff out, edit this post directly, and have fun!</p>
+<p>This is a hands-on demo post to familiarise yourself with Newsletter Glue. </p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><strong>What you'll learn:</strong></p>
+<p>Edit this post directly and play around.🤸‍♂️</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:list {"ordered":true} -->
-<ol><li>How to send a post to your email subscribers <br>(1 minute)<br></li><li>How to use the WordPress editor to customise your newsletters<br>(10 minutes: feel free to come back later for this part)</li></ol>
-<!-- /wp:list -->
+<!-- wp:paragraph -->
+<p><strong>Let's start!</strong></p>
+<!-- /wp:paragraph -->
 
-<!-- wp:image {"width":418,"height":314} -->
-<figure class="wp-block-image is-resized"><img src="https://winonarasheed.files.wordpress.com/2014/06/get-ready-set-go-read.jpg" alt="" width="418" height="314"/></figure>
+<!-- wp:paragraph -->
+<p>Scroll down till you see this👇 . Then, try sending this post as a test email.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":398,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/check-the-box-1.png" alt="" class="wp-image-398"/></figure>
 <!-- /wp:image -->
-
-<!-- wp:heading -->
-<h2>Part 1: send your first email </h2>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph -->
-<p>Watch the video again or see the step-by-step guide below. </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Then try it for yourself!</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:core-embed/youtube {"url":"https://youtu.be/0LiLb3KKarE","type":"video","providerNameSlug":"youtube","className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
-<figure class="wp-block-embed-youtube wp-block-embed is-type-video is-provider-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
-https://youtu.be/0LiLb3KKarE
-</div></figure>
-<!-- /wp:core-embed/youtube -->
 
 <!-- wp:heading {"level":3} -->
-<h3>Let's begin by sending this post as a test email</h3>
+<h3><strong>Go ahead, try it now!</strong></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>Scroll to the end of the post, look for this section, and check the box.</p>
+<p>Then come back and create a reusable newsletter footer.</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:image {"id":326,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/check-the-box.png" alt="" class="wp-image-326"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:paragraph -->
-<p>Then, look for <strong>Send test email to</strong>, double check your email address and hit <strong>Send</strong>.</p>
+<!-- wp:paragraph {"customBackgroundColor":"#f1ecff","fontSize":"small","className":"NGpurplebox"} -->
+<p style="background-color:#f1ecff" class="has-background has-small-font-size NGpurplebox"><strong>👀</strong> Need help? <a rel="noreferrer noopener" href="https://docs.memberhero.pro/article/14-send-blog-post-to-email-list" target="_blank">Read the step-by-step guide</a></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:image {"id":327,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/send-test-email.png" alt="" class="wp-image-327"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:paragraph -->
-<p>All done? You should receive your test email shortly. </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:heading {"level":3} -->
-<h3>Now, let's publish. </h3>
-<!-- /wp:heading -->
-
-<!-- wp:list -->
-<ul><li><strong>Send as newsletter</strong> should already be checked.</li><li>Check your <strong>segment/tag</strong> to double check who you're sending this post to.</li><li>The green<strong> Your email is ready to publish</strong> shows you're ready...</li></ul>
-<!-- /wp:list -->
-
-<!-- wp:image {"id":370,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/ready-to-publish1.png" alt="" class="wp-image-370"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:list -->
-<ul><li>then hit <strong>Publish</strong> like you normally do.</li></ul>
-<!-- /wp:list -->
-
-<!-- wp:image {"id":371,"width":281,"height":105,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large is-resized"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/publish-button2.png" alt="" class="wp-image-371" width="281" height="105"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:paragraph -->
-<p>That's it. You've now sent your first post as a newsletter! 🎉</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>For more information on Newsletter Glue features, <a rel="noreferrer noopener" href="https://docs.memberhero.pro/collection/7-newsletter-glue" target="_blank">head to our knowledge base</a>.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:image -->
-<figure class="wp-block-image"><img src="https://media2.giphy.com/media/m9eG1qVjvN56H0MXt8/giphy.gif?cid=ecf05e47nesiyecr9n4s9skvrfa6mjpt5meo06pm4adnxvr7&amp;rid=giphy.gif" alt="Leaving See Ya GIF by MOODMAN"/></figure>
-<!-- /wp:image -->
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
 
 <!-- wp:heading -->
-<h2>Part 2: Customising and publishing your newsletter</h2>
+<h2>Create a reusable newsletter footer.</h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"customBackgroundColor":"#f1ecff","fontSize":"small","className":"NGpurplebox"} -->
-<p style="background-color:#f1ecff" class="has-background has-small-font-size NGpurplebox">💡<strong>If you're busy right now, come back later. </strong><br>This post can be found in your WordPress admin. And, if you've sent yourself a test email, you can also read it in your inbox.</p>
+<!-- wp:paragraph -->
+<p>See this <strong>Before you go</strong> section👇? </p>
 <!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>We're going to turn it into a reusable newsletter footer.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Scroll down for instructions.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+<!-- wp:heading {"level":3} -->
+<h3>Before you go</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Here are a few things you can do if you enjoyed reading this newsletter:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>💌 <strong>Become a subscriber:&nbsp;</strong>[link to your sign up page]</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>🗄 Explore past issues:&nbsp;</strong>[link to your newsletter archive]</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>👉 Forward this newsletter&nbsp;</strong>to friends who might enjoy it</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>🌈 Get in touch/Share cool Internet stuff</strong>: [link to your email]</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>➕&nbsp;<strong>Follow me on Twitter:</strong>&nbsp;[link to your twitter profile]</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+<!-- wp:heading {"level":3} -->
+<h3>Steps to make a reusable footer</h3>
+<!-- /wp:heading -->
 
 <!-- wp:heading {"level":4} -->
-<h4>Here's a reusable block. Let's learn how to edit it...👇</h4>
+<h4>1. Fill in your details directly into the section above☝️</h4>
 <!-- /wp:heading -->
 
-<!-- wp:block {"ref":380} /-->
-
-<!-- wp:paragraph -->
-<p>Edit the text inside by first <strong>converting it to a regular block</strong>. </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Click on it to select the block, then follow the steps below.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>After that, you can update the text to whatever you want. This won't affect the reusable block template. </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:image {"id":342,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/convert-to-regular-block1.png" alt="" class="wp-image-342"/></figure>
+<!-- wp:image {"id":399,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/before-you-go-fill-out-deets.png" alt="" class="wp-image-399"/></figure>
 <!-- /wp:image -->
-
-<!-- wp:paragraph -->
-<p>Did you do it? Great! 😻</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Next, let's insert a reusable block by typing <code>/ng</code> and selecting <strong>NG yellow box </strong>.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:image {"id":355,"width":409,"height":295,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large is-resized"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/NG-yellow-box1.png" alt="" class="wp-image-355" width="409" height="295"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:paragraph -->
-<p>You should see a yellow box with a quote from Kurt Vonnegut. </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Got it? Let's keep going! 💃</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p></p>
-<!-- /wp:paragraph -->
 
 <!-- wp:heading {"level":4} -->
-<h4>Now, let's save this table as a reusable block. 👇</h4>
+<h4>2. Add to reusable blocks</h4>
 <!-- /wp:heading -->
 
-<!-- wp:table {"hasFixedLayout":true} -->
-<figure class="wp-block-table"><table class="has-fixed-layout"><tbody><tr><td><img src="https://images.unsplash.com/photo-1445307806294-bff7f67ff225?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80" alt="woman on hammock near to river"></td><td>Don't you wish this were you right now?</td></tr></tbody></table></figure>
-<!-- /wp:table -->
-
-<!-- wp:paragraph -->
-<p>First, click the table, then follow these steps:</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:image {"id":360,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/add-to-reusable-blocks1-1024x616.png" alt="" class="wp-image-360"/></figure>
+<!-- wp:image {"id":414,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/before-you-go-select-all1.png" alt="" class="wp-image-414"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:image {"id":364,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/save-reusable-blocks1-1024x651.png" alt="" class="wp-image-364"/></figure>
+<!-- wp:image {"id":415,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/before-you-go-add-reusable-block1.png" alt="" class="wp-image-415"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:paragraph -->
-<p>Awesome! You've now saved this table as a reusable block. You can use it in your next newsletter by typing <code>/ng</code> and selecting <strong>/NG table</strong>.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:image {"id":374,"width":349,"height":277,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large is-resized"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/NG-table.png" alt="" class="wp-image-374" width="349" height="277"/></figure>
+<!-- wp:image {"id":416,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/before-you-go-name-and-save2.png" alt="" class="wp-image-416"/></figure>
 <!-- /wp:image -->
-
-<!-- wp:paragraph -->
-<p>Don't forget to convert it to a regular block before editing it.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:image {"id":386,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/convert-table-to-regular-block.png" alt="" class="wp-image-386"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:paragraph {"customBackgroundColor":"#f1ecff","fontSize":"small","className":"NGpurplebox"} -->
-<p style="background-color:#f1ecff" class="has-background has-small-font-size NGpurplebox">💡 <strong>Advanced users: Add custom CSS to a block</strong><br>If you're comfortable with CSS, head to <strong>Newsletter Glue </strong>&gt; <strong>Settings</strong> &gt; <strong>Custom CSS tab</strong> to add in-line CSS to your email. <br><br>Don't forget to give your block a CSS class in the WordPress editor. <a href="https://docs.memberhero.pro/article/6-custom-css" target="_blank" rel="noreferrer noopener">Learn more</a></p>
-<!-- /wp:paragraph -->
 
 <!-- wp:heading {"level":4} -->
-<h4>Finally... Here's a footer reusable block. You can see it's missing some information. Let's fill it out.👇</h4>
+<h4>3. Use it!</h4>
 <!-- /wp:heading -->
 
-<!-- wp:block {"ref":320} /-->
-
-<!-- wp:paragraph -->
-<p>In this case, you'll want your footer to be the same across <strong>all your newsletters</strong>. </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>So you want to click <strong>edit</strong> to edit the reusable block and permanently change the block.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:image {"id":346,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/edit-before-you-go.png" alt="" class="wp-image-346"/></figure>
+<!-- wp:image {"id":406,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/type-footer.png" alt="" class="wp-image-406"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:paragraph -->
-<p>Once you've added your information, simply <strong>save</strong> the reusable block. And you're done.</p>
+<p>Add your new footer to the bottom of this post. </p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p>In the future, you can add this reusable block to the bottom of each post. Simply type <code>/ng</code> to find it.</p>
+<p>Check it out by sending yourself another test email. </p>
 <!-- /wp:paragraph -->
 
-<!-- wp:image {"id":358,"width":385,"height":323,"sizeSlug":"large"} -->
-<figure class="wp-block-image size-large is-resized"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/NG-before-you-go1.png" alt="" class="wp-image-358" width="385" height="323"/></figure>
+<!-- wp:paragraph -->
+<p>Or, if you're ready, hit <strong>Publish...</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":408,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/publish-button3.png" alt="" class="wp-image-408"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:paragraph -->
-<p>Since you don't need to edit the content of the block, there's no need to convert it to a regular block.</p>
+<p>You should see this in the Newsletter Glue section...</p>
 <!-- /wp:paragraph -->
+
+<!-- wp:image {"id":411,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://wordpress-280578-1189454.cloudwaysapps.com/wp-content/uploads/2020/08/Screenshot-2020-08-07-at-11.51.23-AM.png" alt="" class="wp-image-411"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
 
 <!-- wp:heading {"level":3} -->
 <h3>The end!</h3>
 <!-- /wp:heading -->
-
-<!-- wp:paragraph -->
-<p>Hope you found this tutorial useful. </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Now you have everything you need to create your latest newsletter with the reusable blocks. </p>
-<!-- /wp:paragraph -->
 
 <!-- wp:image -->
 <figure class="wp-block-image"><img src="https://media2.giphy.com/media/QW5nKIoebG8y4/giphy.gif" alt="Cheer Applause GIF by Peanuts"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:paragraph -->
-<p>To learn even more tips for publishing newsletters using the WordPress editor, check out <a href="https://memberhero.pro/blog/feature-tutorials/gutenberg-editor-tips/" target="_blank" rel="noreferrer noopener">this post</a>. </p>
+<p>If you'd like to keep playing around, try sending one of your older posts as a newsletter. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Have fun!</p>
 <!-- /wp:paragraph -->
