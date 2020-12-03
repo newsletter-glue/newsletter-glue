1b1b671ab45681327448303efd38508aa3a73231
newsletter-glue <68359897+newsletter-glue@users.noreply.github.com>
2020-12-03 20:15:07 +0100

    Update messages.php

--- includes/admin/metabox/views/messages.php
+++ includes/admin/metabox/views/messages.php
@@ -17,8 +17,8 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 			<label>
 				<input type="checkbox" name="ngl_double_optin" id="ngl_double_optin" value="1" checked />
 				<?php _e( 'Send email when post is published.', 'newsletter-glue' ); ?>
-				<span class="ngl-new-tag"><?php _e( 'New', 'newsletter-glue' ); ?></span>
 			</label>
+			<span class="ngl-new-tag"><?php _e( 'New', 'newsletter-glue' ); ?></span>
 		</div>
 	</div>
 	<?php endif; ?>
@@ -30,8 +30,8 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 			<label>
 				<input type="checkbox" name="ngl_double_optin" id="ngl_double_optin" value="1" />
 				<?php _e( 'Send email when post is updated.', 'newsletter-glue' ); ?>
-				<span class="ngl-new-tag"><?php _e( 'New', 'newsletter-glue' ); ?></span>
 			</label>
+			<span class="ngl-new-tag"><?php _e( 'New', 'newsletter-glue' ); ?></span>
 		</div>
 	</div>
 	<?php endif; ?>
