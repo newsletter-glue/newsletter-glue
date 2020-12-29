27c73ebc0861a70c40e363e9bf16f514caeb66e5
newsletter-glue <68359897+newsletter-glue@users.noreply.github.com>
2020-12-29 21:49:56 +0100

    Add twitter, youtube embeds

--- includes/pro.php
+++ includes/pro.php
@@ -40,6 +40,12 @@ class NGL_Pro {
 
 		// Admin notice.
 		add_action( 'admin_notices', array( $this, 'admin_notice' ), 120 );
+
+		// Social embeds.
+		add_filter( 'newsletterglue_generate_content', array( $this, 'social_embeds' ), 100, 2 );
+
+		// Custom CSS.
+		add_action( 'newsletterglue_email_styles', array( $this, 'embed_css' ), 50 );
 	}
 
 	/**
@@ -291,6 +297,231 @@ class NGL_Pro {
 			<p class="ngl-notice-logo"><?php _e( 'Thanks for purchasing Newsletter Glue! Get frequent updates as we improve the plugin.', 'newsletter-glue' ); ?></p>
 			<p><a href="<?php echo admin_url( 'admin.php?page=ngl-settings&tab=license' ); ?>"><?php _e( 'Add license key to get updates', 'newsletter-glue' ); ?></a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://newsletterglue.com/account"><?php _e( 'Get license key', 'newsletter-glue' ); ?></a></p>
 		</div>
+		<?php
+	}
+
+	/**
+	 * Display social embeds.
+	 */
+	public function social_embeds( $html, $post ) {
+
+		$dom = new \DOMDocument();
+
+		libxml_use_internal_errors( true );
+
+		$dom->loadHTML( mb_convert_encoding( $html, 'HTML-ENTITIES', 'UTF-8') );
+
+		libxml_clear_errors();
+
+		$finder = new \DOMXPath( $dom );
+
+		// Embed Twitter.
+		$nodes = $finder->query( "//*[contains(concat(' ', normalize-space(@class), ' '), ' is-provider-twitter ')]" );
+		if ( ! empty( $nodes ) ) {
+			foreach ( $nodes as $node ) {
+				$html = $this->get_tweet( $node->nodeValue );
+				if ( $html ) {
+					$twitter = $dom->createElement( 'div', $html );
+					$twitter->setAttribute( 'class', 'ngl-embed-social ngl-embed-twitter' );
+					$node->parentNode->replaceChild( $twitter, $node );
+				} else {
+					$node->parentNode->removeChild( $node );
+				}
+			}
+		}
+
+		// Embed YouTube.
+		$nodes = $finder->query( "//*[contains(concat(' ', normalize-space(@class), ' '), ' is-provider-youtube ')]" );
+		if ( ! empty( $nodes ) ) {
+			foreach ( $nodes as $node ) {
+				$html = $this->get_youtube( $node->nodeValue );
+				if ( $html ) {
+					$youtube = $dom->createElement( 'div', $html );
+					$youtube->setAttribute( 'class', 'ngl-embed-social ngl-embed-youtube' );
+					$node->parentNode->replaceChild( $youtube, $node );
+				} else {
+					$node->parentNode->removeChild( $node );
+				}
+			}
+		}
+
+		return htmlspecialchars_decode( $dom->saveHTML() );
+
+	}
+
+	/**
+	 * Get youtube.
+	 */
+	public function get_youtube( $url ) {
+
+		$url = urlencode( untrailingslashit( trim( $url ) ) );
+
+		$request  = wp_remote_get( 'https://www.youtube.com/oembed?url=' . $url );
+		$response = wp_remote_retrieve_body( $request );
+
+		$data = json_decode( $response );
+
+		if ( empty( $data ) ) {
+			return false;
+		}
+
+		$image_url = str_replace( 'hqdefault', 'maxresdefault', $data->thumbnail_url );
+
+		$html = '<a href="' . urldecode( trim( $url ) ) . '" target="_blank"><img src="' . $image_url . '" class="ngl-embed-youtube-thumb" /></a>';
+		$html .= '<a href="' . urldecode( trim( $url ) ) . '" target="_blank">' . $data->title . '</a>';
+
+		$html .= '<div class="ngl-embed-meta">
+					<div class="ngl-embed-metadata">
+						<strong>' . $data->author_name . '</strong><br />
+						<a href="' . $data->author_url . '" target="_blank">' . __( 'Go to channel &#8599;', 'newsletter-glue' ) . '</a>
+					</div>
+					<div class="ngl-embed-icon">
+						<a href="https://youtube.com/" target="_blank"><img src="' . NGL_PLUGIN_URL . '/assets/images/social/youtube.png" /></a>
+					</div>
+				</div>';
+
+		return $html;
+
+	}
+
+	/**
+	 * Get tweet.
+	 */
+	public function get_tweet( $url ) {
+
+		$url = urlencode( untrailingslashit( trim( $url ) ) );
+
+		$request  = wp_remote_get( 'https://publish.twitter.com/oembed?omit_script=true&url=' . $url );
+		$response = wp_remote_retrieve_body( $request );
+
+		$data = json_decode( $response );
+
+		if ( empty( $data->html ) ) {
+			return false;
+		}
+
+		$html = preg_replace( '#<script(.*?)>(.*?)</script>#is', '', ( string ) trim( $data->html ) );
+		$html = str_replace( 'blockquote', 'div', trim( $html ) );
+
+		$stripped = preg_replace( '/<p\b[^>]*>(.*?)<\/p>/i', '', $html );
+		preg_match( '#<a(.*?)</a>#i', $stripped, $match );
+		$date = wp_strip_all_tags( $match[0] );
+		$formatted_date = date_i18n( 'M j, Y', strtotime( $date ) );
+
+		preg_match( '%(<p[^>]*>.*?</p>)%i', $html, $regs );
+		$html = $regs[0];
+
+		if ( preg_match("/^https?:\/\/(www\.)?twitter\.com\/(#!\/)?(?<name>[^\/]+)(\/\w+)*$/", $data->author_url, $regs ) ) {
+			$username = '<a href="' . $data->author_url . '" target="_blank">@' . $regs[ 'name' ] . '</a>';
+		} else {
+			$username = '<a href="' . $data->author_url . '" target="_blank">' . $data->author_url . '</a>';
+		}
+
+		$html .= '<div class="ngl-embed-date">' . $formatted_date . '</div>';
+		$html .= '<div class="ngl-embed-meta">
+					<div class="ngl-embed-metadata">
+						<strong>' . $data->author_name . '</strong><br>
+						' . $username . '
+					</div>
+					<div class="ngl-embed-icon">
+						<a href="https://twitter.com/" target="_blank"><img src="' . NGL_PLUGIN_URL . '/assets/images/social/twitter.png" /></a>
+					</div>
+				</div>';
+
+		return $html;
+
+	}
+
+	/**
+	 * Embed CSS.
+	 */
+	public function embed_css() {
+		?>
+		.ngl-embed-social {
+			background: #fff !important;
+			border: 1px solid #b9b9b9;
+			border-radius: 5px;
+			padding: 20px;
+			font-size: 13px;
+			line-height: 1.4;
+			margin: 0 0 25px;
+		}
+
+		.ngl-embed-social p {
+			line-height: 1.4;
+			font-size: 14px;
+			color: #111 !important;
+		}
+
+		.ngl-embed-social a {
+			color: rgb(27, 149, 224) !important;
+		}
+
+		.ngl-embed-meta {
+			display: flex;
+			border-top: 1px solid rgb(204, 214, 221);
+			line-height: 1.4;
+			padding: 12px 0 0;
+			margin: 12px 0 0;
+		}
+
+		.ngl-embed-metadata {
+			display: inline-block;
+			text-align: left;
+		}
+
+		.ngl-embed-metadata strong {
+			font-weight: bold;
+			color: #111;
+		}
+
+		.ngl-embed-icon {
+			display: inline-block;
+			text-align: right;
+			margin-left: auto;
+		}
+
+		.ngl-embed-icon img {
+			width: 30px !important;
+			height: 30px !important;
+			margin: 0 !important;
+		}
+
+		.ngl-embed-date,
+		.ngl-embed-date a {
+			font-size: 12px;
+			color: rgb(91, 112, 131) !important;
+			text-decoration: none !important;
+		}
+
+		.ngl-embed-twitter {
+			background: #fff !important;
+			border-color: rgb(204, 214, 221);
+			color: #111 !important;
+		}
+
+		.ngl-embed-twitter p {
+			margin: 0 0 8px !important;
+		}
+
+		.ngl-embed-twitter .ngl-embed-metadata a {
+			color: rgb(91, 112, 131) !important;
+			text-decoration: none !important;
+		}
+
+		.ngl-embed-youtube a {
+			color: #ff0000 !important;
+			text-decoration: none !important;
+		}
+
+		.ngl-embed-youtube-thumb {
+			margin: 0 0 12px !important;
+			border-radius: 5px !important;
+		}
+
+		.ngl-embed-youtube .ngl-embed-metadata a {
+			color: #999 !important;
+		}
 		<?php
 	}
 
