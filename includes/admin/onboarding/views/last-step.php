a4686785bf5b6e6f49745025f1a1073aa7ffd87c
newsletter-glue <68359897+newsletter-glue@users.noreply.github.com>
2021-03-05 16:10:28 +0200

    optimised onboarding steps

--- includes/admin/onboarding/views/last-step.php
+++ includes/admin/onboarding/views/last-step.php
@@ -8,29 +8,29 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 ?>
 
-<div class="ngl-boarding alt is-hidden" data-screen="4">
+<div class="ngl-boarding alt is-hidden" data-screen="5">
 
 	<div class="ngl-boarding-logo">
 		<div class="ngl-logo"><img src="<?php echo NGL_PLUGIN_URL . '/assets/images/top-bar-logo.svg'; ?>" /></div>
 	</div>
 
-	<div class="ngl-boarding-step"><?php _e( 'Step 2 of 3', 'newsletter-glue' ); ?></div>
+	<div class="ngl-boarding-step"><?php _e( 'Step 3 of 3', 'newsletter-glue' ); ?></div>
 
-	<h3 style="max-width:100%;"><?php _e( 'Now, please enter your default brand ID and lists.', 'newsletter-glue' ); ?>
+	<h3 style="max-width:100%;"><?php _e( 'Last step! Personalise your emails.', 'newsletter-glue' ); ?>
 		<span><?php _e( 'You can always change this later on in the settings.', 'newsletter-glue' ); ?></span>
 	</h3>
 
 	<div class="ngl-settings ngl-metabox-flex">
 		<div class="ngl-metabox-header">
-			<?php esc_html_e( 'List ID(s)', 'newsletter-glue' ); ?>
-			<?php $api->input_verification_info(); ?>
+			<?php esc_html_e( 'From name', 'newsletter-glue' ); ?>
+			<?php $this->input_verification_info(); ?>
 		</div>
 		<div class="ngl-field">
 			<?php
 				newsletterglue_text_field( array(
-					'id' 			=> 'ngl_lists',
-					'helper'		=> __( 'Comma-separated list of lists IDs. List ID is hashed, alpha-numeric.', 'newsletter-glue' ),
-					'value'			=> newsletterglue_get_option( 'lists', $app ),
+					'id' 			=> 'ngl_from_name',
+					'helper'		=> __( 'Your subscribers will see this name in their inboxes.', 'newsletter-glue' ),
+					'value'			=> newsletterglue_get_option( 'from_name', $app ),
 					'class'			=> 'ngl-ajax',
 				) );
 			?>
@@ -39,15 +39,19 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 	<div class="ngl-settings ngl-metabox-flex">
 		<div class="ngl-metabox-header">
-			<?php esc_html_e( 'Brand ID', 'newsletter-glue' ); ?>
-			<?php $api->input_verification_info(); ?>
+			<?php esc_html_e( 'From email', 'newsletter-glue' ); ?>
+			<?php $this->email_verification_info(); ?>
 		</div>
 		<div class="ngl-field">
 			<?php
+				$email = newsletterglue_get_option( 'from_email', $app );
+				if ( ! $email ) {
+					$email = get_option( 'admin_email' );
+				}
 				newsletterglue_text_field( array(
-					'id' 			=> 'ngl_brand',
-					'helper'		=> __( 'You can find the numeric brand ID in your Sendy dashboard.', 'newsletter-glue' ),
-					'value'			=> newsletterglue_get_option( 'brand', 'sendy' ),
+					'id' 			=> 'ngl_from_email',
+					'helper'		=> __( 'Subscribers will see and reply to this email address.', 'newsletter-glue' ),
+					'value'			=> $email,
 					'class'			=> 'ngl-ajax',
 				) );
 			?>
@@ -58,5 +62,3 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	<div class="ngl-boarding-prev"><span class="material-icons">arrow_back</span><span class="ngl-boarding-prev-text"><?php _e( 'prev', 'newsletter-glue' ); ?></span></div>
 
 </div>
-
-<?php $api->load_last_onboarding_screen(); ?>
