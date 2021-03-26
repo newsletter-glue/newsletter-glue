f9f820b1e61bb43bf0deda2a26128cab8ba6c779
newsletter-glue <68359897+newsletter-glue@users.noreply.github.com>
2021-03-26 20:03:58 +0200

    Upgrades

---
+++ includes/upgrade.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Upgrade.
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * NGL_Upgrade class.
+ */
+class NGL_Upgrade {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+
+	}
+
+}
+
+return new NGL_Upgrade;
