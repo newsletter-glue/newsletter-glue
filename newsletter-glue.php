86e060d199db78a120f2bcb0d8b3534f975f79c7
newsletter-glue <68359897+newsletter-glue@users.noreply.github.com>
2021-04-06 17:30:43 +0200

    Update newsletter-glue.php

--- newsletter-glue.php
+++ newsletter-glue.php
@@ -5,7 +5,7 @@
  * Description: Email posts to subscribers from the WordPress editor. Works with Mailchimp, MailerLite, Sendinblue…
  * Author: Newsletter Glue
  * Author URI: https://newsletterglue.com
- * Version: 1.2.1
+ * Version: 1.2.3
  * Text Domain: newsletter-glue
  * Domain Path: /i18n/languages/
  */
@@ -71,7 +71,7 @@ final class Newsletter_Glue {
 
 		// Plugin version.
 		if ( ! defined( 'NGL_VERSION' ) ) {
-			define( 'NGL_VERSION', '1.2.1' );
+			define( 'NGL_VERSION', '1.2.3' );
 		}
 
 		// Plugin Folder Path.
