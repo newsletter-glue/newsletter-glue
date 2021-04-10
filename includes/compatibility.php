a2b22fa54ed137afda77d78b6a85030a8ebfef96
newsletter-glue <68359897+newsletter-glue@users.noreply.github.com>
2021-04-10 04:00:47 +0200

    Update compatibility.php

--- includes/compatibility.php
+++ includes/compatibility.php
@@ -11,10 +11,24 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 function newsletterglue_duplicate_post_excludelist_filter( $meta_excludelist ) {
 
-	return array_merge( $meta_excludelist, [ '_ngl_results', '_newsletterglue' ] );
+	return array_merge( $meta_excludelist, [ '_ngl_results' ] );
 
 }
 add_filter( 'duplicate_post_excludelist_filter', 'newsletterglue_duplicate_post_excludelist_filter' );
+
+/**
+ * Performs some actions after the WordPress standard fields of a post, or a non-hierarchical custom type item, have been copied.
+ */
+function newsletterglue_custom_dp_duplicate_post( $new_post_id, $post, $status ) {
+
+	$meta = get_post_meta( $new_post_id, '_newsletterglue', true );
+	if ( ! empty( $meta ) && isset( $meta[ 'sent' ] ) ) {
+		unset( $meta[ 'sent' ] );
+		update_post_meta( $new_post_id, '_newsletterglue', $meta );
+	}
+
+}
+add_action( 'dp_duplicate_post', 'newsletterglue_custom_dp_duplicate_post', 999, 3 );
 
 /**
  * Fix conflict with MailPoet css.
